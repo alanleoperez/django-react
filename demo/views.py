caa086e0e7cb407bc779d98226f2f960fdaf1179
Alan L. Perez <aperez@sorensoneng.com>
2025-03-25 16:25:58 -0700

    section 2: django introduction

--- demo/views.py
+++ demo/views.py
@@ -5,15 +5,6 @@ from django.views.generic import View
 from .models import Book
 
 
-class Another(View):
-    book = Book.objects.get(id=1)
-    output = f"We have {book.title} book with ID {book.id}<br>"
-    # for book in books:
-    #     output += f"We have {book.title} book with ID {book.id}<br>"
-
-    def get(self, request):
-        return HttpResponse(self.output)
-
-
 def first(request):
-    return render("First message from views")
+    books = Book.objects.all()
+    return render(request, "First_temp.html", {"books": books})
