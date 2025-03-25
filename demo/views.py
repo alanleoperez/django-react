0f155dd2f8334816f893c1f6ec40954d0fbecaa9
Alan L. Perez <aperez@sorensoneng.com>
2025-03-25 16:39:22 -0700

    update progress on serializers

--- demo/views.py
+++ demo/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import View
+from rest_framework import viewsets
 
 from .models import Book
+from .serializers import BookSerializer
 
 
-def first(request):
-    books = Book.objects.all()
-    return render(request, "First_temp.html", {"books": books})
+class BookViewSet(viewsets.ModelViewSet):
+    serializer_class = BookSerializer
